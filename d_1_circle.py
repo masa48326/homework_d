07c27af4d4af2e705290220dbc5d83fca437429a
shibatamasayuki <masayuki.shibata.0831@gmail.com>
2019-11-07 14:32:47 +0900

    D-1 完成

--- d_1_circle.py
+++ d_1_circle.py
@@ -22,7 +22,17 @@ class Circle:
         self.radius = radius
 
     def area(self):
-        return int(radius ** 2 * math.pi * 100) / 100
+        return int(self.radius ** 2 * math.pi * 100) / 100
 
     def perimeter(self):
         return int(self.radius * 2 * math.pi * 100) / 100
+
+
+circle1 = Circle(radius=1)
+print(circle1.area())  # 3.14
+print(circle1.perimeter())  # 6.28
+
+# 半径3の円
+circle3 = Circle(radius=3)
+print(circle3.area())  # 28.27
+print(circle3.perimeter())  # 1 8.85
